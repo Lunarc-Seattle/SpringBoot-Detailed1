3b5c6c2cb4a549a9f8588eee681ee06c1c70f28d
Lunarc-Seattle <994527498@qq.com>
2025-05-19 17:43:55 +0800

    structured as springboot

---
+++ demo222222/src/main/java/com/fia/demo222222/Demo222222Application.java
@@ -0,0 +1,13 @@
+package com.fia.demo222222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo222222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo222222Application.class, args);
+    }
+
+}
---
+++ demo222222/src/test/java/com/fia/demo222222/Demo222222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fia.demo222222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo222222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
