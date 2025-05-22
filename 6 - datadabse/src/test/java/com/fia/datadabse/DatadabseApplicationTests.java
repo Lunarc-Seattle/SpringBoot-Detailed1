f901611ab7165811f550ea257a96f6b06a9b39e3
Lunarc-Seattle <994527498@qq.com>
2025-05-22 21:09:54 +0800

    added database

---
+++ 6 - datadabse/src/test/java/com/fia/datadabse/DatadabseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fia.datadabse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatadabseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
