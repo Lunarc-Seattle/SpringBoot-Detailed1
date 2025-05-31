d9ecfa489cf81c7749542f3534effeafb48b791e
Lunarc-Seattle <994527498@qq.com>
2025-05-31 11:27:13 +0800

    connect to H2

--- 6 - datadabse/src/main/java/com/fia/datadabse/DatadabseApplication.java
+++ 6 - datadabse/src/main/java/com/fia/datadabse/DatadabseApplication.java
@@ -1,13 +1,29 @@
 package com.fia.datadabse;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class DatadabseApplication {
+@Log
+public class DatadabseApplication implements CommandLineRunner {
+	private final DataSource dataSource;
+	public DatadabseApplication(final DataSource dataSource) {this.dataSource = dataSource;}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatadabseApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args)  {
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
 	}
 
 }
